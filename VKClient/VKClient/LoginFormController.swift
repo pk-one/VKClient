e336b13ea9c7b70dd60e5768eb6f26432d4014d0
pK <pave.kruchin@gmail.com>
2021-07-17 14:28:50 +0200

    fixed login form

--- VKClient/VKClient/LoginFormController.swift
+++ VKClient/VKClient/LoginFormController.swift
@@ -10,59 +10,57 @@ import Foundation
 
 class LoginFormController: UIViewController {
     
-    @IBOutlet weak var UIScrollView: UIScrollView!
-    @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var signInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        customLoginTextField()
+        customPasswordTextField()
+        customButtonSignIn()
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+    }
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillBeHidden(notification: Notification){
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+    }
+    
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+    
+    func customLoginTextField() {
         loginTextField.attributedPlaceholder = NSAttributedString(string: "Логин",
                                                                   attributes: [NSAttributedString.Key.foregroundColor: UIColor.brandGrey])
-        
         loginTextField.clipsToBounds = true
         loginTextField.textColor = UIColor.brandGrey
         loginTextField.roundCorners([.topLeft, .topRight], radius: 8)
         loginTextField.addBottomBorderWithColor(color: UIColor.lightGray, width: 0.5)
-        
-        
-        
+    }
+    
+    func customPasswordTextField() {
         passwordTextField.isSecureTextEntry = true
         passwordTextField.attributedPlaceholder = NSAttributedString(string: "Пароль",
                                                                      attributes: [NSAttributedString.Key.foregroundColor: UIColor.brandGrey])
         passwordTextField.textColor = UIColor.brandGrey
         passwordTextField.roundCorners([.bottomLeft, .bottomRight], radius: 8)
-        
+    }
+    
+    func customButtonSignIn() {
         signInButton.clipsToBounds = true
         signInButton.layer.cornerRadius = 8
-        
-        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        UIScrollView?.addGestureRecognizer(hideKeyboardGesture)
-    }
-    
-    
-   
-    
-    @objc func keyboardWasShown(notification: Notification) {
-        
-        
-        let info = notification.userInfo! as NSDictionary
-        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
-        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
-        
-        self.UIScrollView?.contentInset = contentInsets
-        UIScrollView?.scrollIndicatorInsets = contentInsets
-    }
-    
-    @objc func keyboardWillBeHidden(notification: Notification){
-        let contentInsets = UIEdgeInsets.zero
-        UIScrollView?.contentInset = contentInsets
-        
-    }
-    
-    @objc func hideKeyboard() {
-        self.UIScrollView?.endEditing(true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -72,17 +70,11 @@ class LoginFormController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    
-    
-    
     @IBAction func IBACtion(_ sender: Any) {
-        
         let login = loginTextField.text
         let password = passwordTextField.text
         
@@ -91,7 +83,6 @@ class LoginFormController: UIViewController {
         } else {
             print("It`s bad")
         }
-        
     }
 }
 
